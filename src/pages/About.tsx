e4b7c9276ecc3f1de27dd39bc49e8b2141d9445b
akncnkoc <akincankoc@gmail.com>
2022-07-31 09:56:40 +0300

    about page changes

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,14 @@ import { FC } from "react";
 
 const About: FC = () => {
   return (
-    <div className="flex flex-1 items-center justify-center bg-blue-100">
-      <h1>
+    <div className="flex flex-1 items-center justify-center bg-blue-100 flex-col space-y-8">
+      <div className="text-xl">
+        This project using
+        <a href="https://www.themoviedb.org/" target="_blank" className="text-blue-600 italic text-lg">
+          THE MOVIE DATABASE API
+        </a>, when using app feel free to change language
+      </div>
+      <h1 className="text-xl">
         This movie project created by{" "}
         <a
           href="https://portfolio-indol-eight-75.vercel.app/"
